8aea22d3317fbced8c37960ba35bcc6b2aa9e8b2
zrpiupiupiu <always.wyz@outlook.com>
2022-09-06 21:24:37 +0800

    props传参渲染及歌单详情背景处理

---
+++ src/components/item/ItemMusicTop.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="itemMusicTop">
+        <img :src="playlist.coverImgUrl" alt="" class="backgroundImg">
+        <div class="itemLeft">
+            <svg class="icon" aria-hidden="true" @click="$router.go(-1)">
+                <use xlink:href="#icon-zuojiantou"></use>
+            </svg>
+            <span class="font">歌单</span>
+        </div>
+
+        <div class="itemRight">
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-sousuo"></use>
+            </svg>
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-liebiao1"></use>
+            </svg>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        setup(props){
+            console.log(props)
+
+        },
+        props:['playlist'],
+    }
+</script>
+
+<style>
+    .itemMusicTop{
+        width: 100%;
+        height: 1rem;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+       
+        position: relative;
+    }
+
+    .backgroundImg{
+        width: 100%;
+        height: 11rem;
+        position: fixed;
+        z-index: -1;
+        filter: blur(30px);
+
+    }
+
+    .itemLeft{
+        width: 30%;
+        height: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin: 0 .2rem;
+
+    }
+
+    .itemRight{
+        width: 25%;
+        height: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin: 0 .2rem;
+
+    }
+
+    .font {
+        font-size: 26px;
+        color:white;
+    }
+
+    .icon{
+        fill: white;
+    }
+
+    
+</style>
--- src/views/ItemMusic.vue
+++ src/views/ItemMusic.vue
@@ -1,16 +1,17 @@
 <template>
-    <div>歌单详情页</div>
+    <ItemMusicTop :playlist="playlist" />
 </template>
 
 <script>
     import { useRoute } from 'vue-router';
     import { onMounted, reactive } from 'vue';
     import {getMusicItemList} from '@/request/api/item.js'
+    import ItemMusicTop from '@/components/item/ItemMusicTop.vue'
     export default {
         setup(){
 
             const state = reactive({
-                playList:{}
+                playlist:{}
             })
             onMounted(async()=>{
                 let id=useRoute().query.id
@@ -18,10 +19,13 @@
                 console.log(useRoute())
                 let res =await getMusicItemList(id);
                 console.log(res)
-                state.playList=res.data.playlist
+                state.playlist=res.data.playlist
             })
 
             return(state)
+        },
+        components:{
+            ItemMusicTop
         }
 
     }
